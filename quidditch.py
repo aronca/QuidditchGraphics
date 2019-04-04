5c17a94b8cd7f2fda0f78eea6a2ade18f4699d0c
Marhatta <r10marh@siena.edu>
2019-04-04 11:20:06 -0400

    added texture coordinates for the mountains

--- quidditch.py
+++ quidditch.py
@@ -35,64 +35,23 @@ class quidditch(viz.EventClass):
 		self.broom.setLocation(self.broom.getX()+10,self.broom.getY()+15,self.broom.getZ()+40)
 		
 		self.mount = viz.addTexture('mountainTexture.png')
+		self.mount.wrap(viz.WRAP_S,viz.REPEAT)
+		self.mount.wrap(viz.WRAP_T,viz.REPEAT)
+		
+		
+		
 		sky = viz.add(viz.ENVIRONMENT_MAP,'sky.jpg')
 
 		skybox = viz.add('skydome.dlc')
 		skybox.texture(sky)
 		
-		#delete this if texture mapping is added
-		self.mtn1 = Model('mtn'+os.sep+'model.dae')
-		self.mtn2 = Model('mtn'+os.sep+'model.dae')
-		self.mtn3 = Model('mtn'+os.sep+'model.dae')
-		self.mtn4 = Model('mtn'+os.sep+'model.dae')
 		
-		
-		
-		self.mtn1.setScale(2)
-		self.mtn1.setLocation(self.mtn1.getX()-60,self.mtn1.getY()-27,self.mtn1.getZ()-26)
-		
-		
-		self.mtn2.setScale(2)
-		self.mtn2.setLocation(self.mtn2.getX()-60,self.mtn2.getY()-27,self.mtn2.getZ()+125)
-		
-		
-		self.mtn3.setScale(2)
-		self.mtn3.setYRotation(90)
-		self.mtn3.setLocation(self.mtn3.getX()-100,self.mtn3.getY()-30,self.mtn3.getZ()+100)
-		
-		
-		self.mtn4.setScale(2)
-		self.mtn4.setYRotation(90)
-		self.mtn4.setLocation(self.mtn4.getX()+100,self.mtn4.getY()-30,self.mtn4.getZ()+100)
 		
 
-		
-		
-		
-		
 		#mount texture coords
-#		viz.startLayer(viz.QUADS)
-#		viz.texCoord(0,1)
-#		viz.vertex(-.5,.5,.5)
-#		viz.texCoord(1,1)
-#		viz.vertex(.5,.5,.5)
-#		viz.texCoord(1,0)
-#		viz.vertex(.5,-.5,.5)
-#		viz.texCoord(0,0)
-#		viz.vertex(-.5,-.5,.5)
-#		viz.vertices=viz.endLayer()
-#		vertices.texture(self.mount)
-	
-#		
-#		s= vizshape.addSphere(radius=50)
-#		self.mount.wrap(viz.WRAP_S,viz.REPEAT)
-#		self.mount.wrap(viz.WRAP_T,viz.REPEAT)
-#		
-#		s.texture(self.mount)
-		
-		
-		
-		
+		cube= viz.addTexQuad()
+		quad.setPosition([-.75,2,3])
+		quad.texture(self.mount)
 		
 		
 	
