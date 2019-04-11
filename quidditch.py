1483f205c3ea22532d3db838f55655acaf03d484
Marhatta <r10marh@siena.edu>
2019-04-11 01:13:00 -0400

    texture mapping and somewhat snitch in scene

--- quidditch.py
+++ quidditch.py
@@ -12,13 +12,7 @@ from Model import *
 class quidditch(viz.EventClass):
 	def __init__(self):
 		
-		#rotate view
-		#self.view=viz.MainView
-		#mat = viz.Matrix()
-		#mat.postAxisAngle(1,0,0,90)
-		#mat.postTrans(0,20,50)
-		#self.view.setMatrix(mat)
-		
+
 		viz.EventClass.__init__(self)
 		
 		self.callback(viz.KEYDOWN_EVENT, self.onKeyDown)
@@ -39,33 +33,25 @@ class quidditch(viz.EventClass):
 		
 		
 		self.quadribol = Model('quadribol'+os.sep+'model.osgb')
-		#self.snitch = Model('snitch'+os.sep+'scene.gltf')
+		self.snitch = Model('snitch'+os.sep+'model.dae')
 		self.broom = Model('broom'+os.sep+'source'+os.sep+'broom.obj')
 		
-		#woodTexture = viz.addTexture('wood.jpeg')
-		#self.broom.texture(woodtexture)
 		
-		#rescale snitch and move into place, doesnt animate as of now
-		#self.snitch.setScale(.001)
-		#self.snitch.setLocation(self.snitch.getX(),self.snitch.getY()+15,self.snitch.getZ()+50)
 		
 		self.broom.setScale(.005)
 		self.broom.setLocation(self.broom.getX()+10,self.broom.getY()+15,self.broom.getZ()+40)
+		self.snitch.setScale(.1)
+		self.snitch.setLocation(self.snitch.getX(),self.snitch.getY()+15,self.snitch.getZ()+50)
 		
-		self.mount = viz.addTexture('mountainTexture.png')
-		self.mount.wrap(viz.WRAP_S,viz.REPEAT)
-		self.mount.wrap(viz.WRAP_T,viz.REPEAT)
 		
 		
 		#texture mapping on sky cube for background
-		sky = viz.add(viz.ENVIRONMENT_MAP,'sky.jpg')
+		sky = viz.add(viz.ENVIRONMENT_MAP,'mountsky\mount.jpg')
 
 		skybox = viz.add('skydome.dlc')
 		skybox.texture(sky)
 		
-		cube= viz.addTexQuad()
-		#quad.setPosition([-.75,2,3])
-		#quad.texture(self.mount)
+		
 		
 	# Key pressed down event code.
 	def onKeyDown(self,key):
